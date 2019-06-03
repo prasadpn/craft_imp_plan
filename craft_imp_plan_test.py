8cb8a4c8b4952fcaad2a2d36a569e33e588a660c
Pankaj.Prasad <pankaj.nprasad@gmail.com>
2019-06-03 20:01:22 +0530

    central repo

--- craft_imp_plan_test.py
+++ craft_imp_plan_test.py
@@ -12,9 +12,23 @@ from craft_imp_plan_migration_related_changes import migration_related_changes
 #Import statements ends
 
 #Function declaration Phase Starts
-def compare_improvement_plans(old_plan, new_plan, plan, imp_plan_section):
+def identify_column_diff(old_list, new_list):
+    """
+    Compares Column Header values and data types
+    """
     old_val = []
     new_val = []
+    for index, (old, new) in enumerate(zip(old_list, new_list)):
+        if old != new:
+            old_val.append(old)
+            new_val.append(new)
+            print(index)
+    return str(old_val), str(new_val)
+
+def compare_improvement_plans(old_plan, new_plan, plan, imp_plan_section):
+    """
+    Compares improvement plans for differences
+    """
     if old_plan.equals(new_plan):
         return pd.DataFrame(
             {
@@ -23,7 +37,7 @@ def compare_improvement_plans(old_plan, new_plan, plan, imp_plan_section):
                 },
             index=['First']
             )
-    elif len(old_plan) != len(new_plan):
+    if len(old_plan) != len(new_plan):
         return pd.DataFrame(
             {
                 'plan': NEWFILENAME, 'section': imp_plan_section,
@@ -32,7 +46,7 @@ def compare_improvement_plans(old_plan, new_plan, plan, imp_plan_section):
                 },
             index=['First']
             )
-    elif len(old_plan.columns) != len(new_plan.columns):
+    if len(old_plan.columns) != len(new_plan.columns):
         return pd.DataFrame(
             {
                 'plan': plan, 'section': imp_plan_section,
@@ -42,19 +56,10 @@ def compare_improvement_plans(old_plan, new_plan, plan, imp_plan_section):
                 },
             index=['First']
             )
-    elif (old_plan.columns == new_plan.columns).all() and any(old_plan.dtypes != new_plan.dtypes):
-        old_plan_columns = list(old_plan.columns.values)
-        new_plan_columns = list(new_plan.columns.values)
-        old_plan_columns_dtypes = list(old_plan.dtypes)
-        new_plan_columns_dtypes = list(new_plan.dtypes)
-        old_col_dtypes = list(map(list, zip(old_plan_columns, old_plan_columns_dtypes)))
-        new_col_dtypes = list(map(list, zip(new_plan_columns, new_plan_columns_dtypes)))
-        for index, (old, new) in enumerate(zip(old_col_dtypes, new_col_dtypes)):
-            if old != new:
-                old_val.append(old)
-                new_val.append(new)
-        old_val_str = str(old_val)
-        new_val_str = str(new_val)
+    if (old_plan.columns == new_plan.columns).all() and any(old_plan.dtypes != new_plan.dtypes):
+        old_col_dtypes = list(map(list, zip(list(old_plan.columns.values), list(old_plan.dtypes))))
+        new_col_dtypes = list(map(list, zip(list(new_plan.columns.values), list(new_plan.dtypes))))
+        old_val_str, new_val_str = identify_column_diff(old_col_dtypes, new_col_dtypes)
         return pd.DataFrame(
             {
                 'plan': plan, 'section': imp_plan_section,
@@ -63,15 +68,10 @@ def compare_improvement_plans(old_plan, new_plan, plan, imp_plan_section):
                 },
             index=['First']
             )
-    elif any(old_plan.columns != new_plan.columns):
+    if any(old_plan.columns != new_plan.columns):
         old_plan_columns = list(old_plan.columns.values)
         new_plan_columns = list(new_plan.columns.values)
-        for index, (old, new) in enumerate(zip(old_plan_columns, new_plan_columns)):
-            if old != new:
-                old_val.append(old)
-                new_val.append(new)
-        old_val_str = str(old_val)
-        new_val_str = str(new_val)
+        old_val_str, new_val_str = identify_column_diff(old_plan_columns, new_plan_columns)
         return pd.DataFrame(
             {
                 'plan': plan, 'section': imp_plan_section,
@@ -80,14 +80,21 @@ def compare_improvement_plans(old_plan, new_plan, plan, imp_plan_section):
                 },
             index=['First']
             )
-    else:
-        return pd.DataFrame(
-            {
-                'plan': plan, 'section': imp_plan_section,
-                'Result': 'Miss Match', 'from': '', 'to': ''
-                },
-            index=['First']
-            )
+    diff_mask = (old_plan != new_plan) & ~(old_plan.isnull() & new_plan.isnull())
+    ne_stacked = diff_mask.stack()
+    changed = ne_stacked[ne_stacked]
+    changed.index.names = ['id', 'col']
+    difference_locations = np.where(diff_mask)
+    return pd.DataFrame(
+        {
+            'plan': NEWFILENAME, 'section': imp_plan_section,
+            'Result': 'Missmatch in data',
+            'from': old_plan.values[difference_locations],
+            'to': new_plan.values[difference_locations]
+            },
+        index=changed.index
+        )
+
 
 def diff_pd(df1, df2, plan, imp_plan_section):
     """Identify differences between two pandas DataFrames"""
@@ -246,7 +253,7 @@ for configSheetReaderItem in CONFIGSHEETREADER:
         ignore_index=True,
         sort=True
         )
-"""
+
     SheetProtectionResult = check_sheet_protection(
         NEWFILEPATH, NEWFILENAME, CONFIG[configSheetReaderItem]['SheetName']
         )
@@ -255,12 +262,12 @@ for configSheetReaderItem in CONFIGSHEETREADER:
         ignore_index=True,
         sort=True
         )
-"""
+
 COMPARERESULTAPPENDED = COMPARERESULTAPPENDED[['plan', 'section', 'Result', 'from', 'to']]
-#SHEETPROTECTIONRESULTAPPENDED = SHEETPROTECTIONRESULTAPPENDED[['plan', 'sheet', 'Result']]
+SHEETPROTECTIONRESULTAPPENDED = SHEETPROTECTIONRESULTAPPENDED[['plan', 'sheet', 'Result']]
 
 print(COMPARERESULTAPPENDED)
-#print(SHEETPROTECTIONRESULTAPPENDED)
+print(SHEETPROTECTIONRESULTAPPENDED)
 #Data Reading & Testing Phase Ends
 
 #Data Output write Phase starts
